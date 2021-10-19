41f8d28b6a1ba58e907873557013fcbb36f7e86f
pellelito <pelle.lofstrand@gmail.com>
2021-10-19 12:25:28 +0200

    Updated calendar functions

--- Controllers/BookingController.cs
+++ Controllers/BookingController.cs
@@ -1,8 +1,13 @@
 ﻿using AspNetCoreHero.ToastNotification.Abstractions;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Calendar.v3;
+using Google.Apis.Calendar.v3.Data;
+using Google.Apis.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using System;
+using System.IO;
 using System.Linq;
 using Web4Spain.Data;
 using Web4Spain.Models;
@@ -16,6 +21,8 @@ namespace Web4Spain.Controllers
         private readonly ILogger<BookingController> _logger;
         private readonly INotyfService _notyf;
         private readonly ApplicationDbContext _context;
+
+
 
         public BookingController(ILogger<BookingController> logger, INotyfService notyf, ApplicationDbContext context)
         {
@@ -35,7 +42,7 @@ namespace Web4Spain.Controllers
         public ActionResult Booking(BookingModel booking)
         {
 
-            if (ModelState.IsValid && checkAvailability(booking))
+            if (ModelState.IsValid && CheckAvailability(booking))
             {
                 booking.BookingId = Guid.NewGuid();
                 booking.HousingId = 1;
@@ -49,7 +56,7 @@ namespace Web4Spain.Controllers
                 _context.Bookings.Add(booking);
                 _context.SaveChanges();
                 ModelState.Clear();
-
+                AddToCalender(booking);
 
             }
             else
@@ -61,6 +68,55 @@ namespace Web4Spain.Controllers
 
             return RedirectToAction(nameof(Booking));
         }
+
+        private static void AddToCalender(BookingModel booking)
+
+        {
+            String calendarId = @"v0sp8kc55n687i5d7rv0f0uqko@group.calendar.google.com";
+            string jsonFile = "web4spain-22b04057a032.json";
+            string[] Scopes = { CalendarService.Scope.Calendar };
+
+
+            ServiceAccountCredential credential;
+
+            using (var stream =
+                new FileStream(jsonFile, FileMode.Open, FileAccess.Read))
+            {
+                var confg = Google.Apis.Json.NewtonsoftJsonSerializer.Instance.Deserialize<JsonCredentialParameters>(stream);
+                credential = new ServiceAccountCredential(
+                   new ServiceAccountCredential.Initializer(confg.ClientEmail)
+                   {
+                       Scopes = Scopes
+                   }.FromPrivateKey(confg.PrivateKey));
+            }
+
+            var service = new CalendarService(new BaseClientService.Initializer()
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Web4Spain",
+            });
+
+            Event calendarEvent = new()
+            {
+                Start = new EventDateTime
+                {
+                    DateTime = booking.ReservationStart,
+                    TimeZone = "Europe/Stockholm"
+                },
+                End = new EventDateTime
+                {
+                    DateTime = booking.ReservationEnd,
+                    TimeZone = "Europe/Stockholm"
+                },
+                Id = booking.BookingId.ToString().Replace("-", ""),
+                Summary = "Booked"
+
+            };
+
+            Event addEvent = service.Events.Insert(calendarEvent, calendarId).Execute();
+            Console.WriteLine("Event created: " + addEvent.ICalUID);
+        }
+
         // GET: BookingController/Details/5
         public ActionResult Details(Guid id)
         {
@@ -111,8 +167,36 @@ namespace Web4Spain.Controllers
         {
             try
             {
+
+
+                String calendarId = @"v0sp8kc55n687i5d7rv0f0uqko@group.calendar.google.com";
+                string jsonFile = "web4spain-22b04057a032.json";
+                string[] Scopes = { CalendarService.Scope.Calendar };
+
+
+                ServiceAccountCredential credential;
+
+                using (var stream =
+                    new FileStream(jsonFile, FileMode.Open, FileAccess.Read))
+                {
+                    var confg = Google.Apis.Json.NewtonsoftJsonSerializer.Instance.Deserialize<JsonCredentialParameters>(stream);
+                    credential = new ServiceAccountCredential(
+                       new ServiceAccountCredential.Initializer(confg.ClientEmail)
+                       {
+                           Scopes = Scopes
+                       }.FromPrivateKey(confg.PrivateKey));
+                }
+
+                var service = new CalendarService(new BaseClientService.Initializer()
+                {
+                    HttpClientInitializer = credential,
+                    ApplicationName = "Web4Spain",
+                });
+
+                service.Events.Delete(calendarId, (booking.BookingId.ToString().Replace("-", ""))).Execute();
                 _context.Remove(booking);
                 _context.SaveChanges();
+
                 return RedirectToAction(nameof(Booking));
             }
             catch
@@ -120,8 +204,15 @@ namespace Web4Spain.Controllers
                 return View();
             }
         }
-        public bool checkAvailability(BookingModel booking)
+        public bool CheckAvailability(BookingModel booking)
         {
+            if (((booking.ReservationEnd - booking.ReservationStart).TotalDays % 7 != 0) || (booking.ReservationStart.DayOfWeek != DayOfWeek.Saturday))
+            {
+                _notyf.Error("Please only select whole weeks, from Saturday to Saturday");
+                return false;
+
+            }
+
             var bookings = _context.Bookings.ToArray();
             if (bookings.Length > 1)
             {
--- Program.cs
+++ Program.cs
@@ -14,6 +14,8 @@ namespace Web4Spain
         public static void Main(string[] args)
         {
             CreateHostBuilder(args).Build().Run();
+
+
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
